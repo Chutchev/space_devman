d7088b9d18f387c2b19137cf5b6cabae27bf5ea1
Chutchev <iChutchev98@gmail.com>
2018-12-29 23:33:44 +0500

    Изменена функция def download_picture()

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -5,16 +5,16 @@ import os
 def parse_collection():
     url = "http://hubblesite.org/api/v3/images"
     params = {"collection_name": "spacecraft"}
-    req = requests.get(url, params=params)
-    for info in req.json():
+    response = requests.get(url, params=params)
+    for info in response.json():
         print(info['id'])
         hubble_parse(info['id'])
 
 
 def hubble_parse(id):
     url = f"http://hubblesite.org/api/v3/image/{id}"
-    req = requests.get(url)
-    image_info = req.json()['image_files'][-2]
+    response = requests.get(url)
+    image_info = response.json()['image_files'][-2]
     image_url = image_info['file_url']
     image_type = image_url.split(".")[-1]
     download_picture(image_url, f"{id}.{image_type}")
@@ -22,15 +22,12 @@ def hubble_parse(id):
 
 
 def download_picture(url, filename):
-    req = requests.get(url)
+    response = requests.get(url)
     path = ".//images"
-    if os.path.exists(path):
-        with open(f"{path}//{filename}", 'wb') as f:
-            f.write(req.content)
-    else:
+    if not os.path.exists(path):
         os.makedirs(path)
-        with open(f"{path}//{filename}", 'wb') as f:
-            f.write(req.content)
+    with open(f"{path}//{filename}", 'wb') as f:
+        f.write(response.content)
 
 
 def main():
--- fetch_spacex.py
+++ fetch_spacex.py
@@ -4,23 +4,20 @@ import os
 
 def fetch_spacex_last_launch():
     url = "https://api.spacexdata.com/v3/launches/latest"
-    req = requests.get(url)
-    info = req.json()
+    response = requests.get(url)
+    info = response.json()
     urls = info['links']['flickr_images']
     for url_number, url in enumerate(urls):
         download_picture(url, f"spacex{url_number}.jpg")
 
 
 def download_picture(url, filename):
-    req = requests.get(url)
+    response = requests.get(url)
     path = ".//images"
-    if os.path.exists(path):
-        with open(f"{path}//{filename}", 'wb') as f:
-            f.write(req.content)
-    else:
+    if not os.path.exists(path):
         os.makedirs(path)
-        with open(f"{path}//{filename}", 'wb') as f:
-            f.write(req.content)
+    with open(f"{path}//{filename}", 'wb') as f:
+        f.write(response.content)
 
 
 def main():
