7e86926736932b11b733f86c7a3cfc92492659eb
Chutchev <iChutchev98@gmail.com>
2018-12-29 23:35:42 +0500

    Убраны лишние print()

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -7,7 +7,6 @@ def parse_collection():
     params = {"collection_name": "spacecraft"}
     response = requests.get(url, params=params)
     for info in response.json():
-        print(info['id'])
         hubble_parse(info['id'])
 
 
@@ -18,7 +17,6 @@ def hubble_parse(id):
     image_url = image_info['file_url']
     image_type = image_url.split(".")[-1]
     download_picture(image_url, f"{id}.{image_type}")
-    print(f"Сохранено. {id}.{image_type}")
 
 
 def download_picture(url, filename):
